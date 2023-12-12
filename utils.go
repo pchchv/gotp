75c12b98bdd5c562b2e0a6eded4f28d62344613c
pchchv <ipchchv@gmail.com>
2023-12-12 17:17:32 +0300

    Implement an Itob function

---
+++ utils.go
@@ -0,0 +1,11 @@
+package otp
+
+// integer to byte array
+func Itob(integer int64) []byte {
+	byteArr := make([]byte, 8)
+	for i := 7; i >= 0; i-- {
+		byteArr[i] = byte(integer & 0xff)
+		integer = integer >> 8
+	}
+	return byteArr
+}
