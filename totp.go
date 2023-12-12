3136301c9bfd6816ee03e0e093c73d63ee45c6df
pchchv <ipchchv@gmail.com>
2023-12-12 17:24:14 +0300

    Implement an AtTime and Now methods

--- totp.go
+++ totp.go
@@ -1,5 +1,7 @@
 package otp
 
+import "time"
+
 // time-based OTP counters.
 type TOTP struct {
 	OTP
@@ -22,4 +24,13 @@ func (t *TOTP) timecode(timestamp int64) int64 {
 // Generate time OTP of given timestamp
 func (t *TOTP) At(timestamp int64) string {
 	return t.generateOTP(t.timecode(timestamp))
+}
+
+func (t *TOTP) AtTime(timestamp time.Time) string {
+	return t.At(timestamp.Unix())
+}
+
+// Generate the current time OTP
+func (t *TOTP) Now() string {
+	return t.At(currentTimestamp())
 }
