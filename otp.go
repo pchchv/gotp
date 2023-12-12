e3b5e2d7e5d68a7438d01055ce594e204f294100
pchchv <ipchchv@gmail.com>
2023-12-12 17:14:34 +0300

    Implement a NewOTP function

--- otp.go
+++ otp.go
@@ -1,6 +1,9 @@
 package otp
 
-import "hash"
+import (
+	"crypto/sha1"
+	"hash"
+)
 
 type Hasher struct {
 	HashName string
@@ -11,4 +14,18 @@ type OTP struct {
 	secret string  // secret in base32 format
 	digits int     // number of integers in the OTP. Some apps expect this to be 6 digits, others support more.
 	hasher *Hasher // digest function to use in the HMAC (expected to be sha1)
+}
+
+func NewOTP(secret string, digits int, hasher *Hasher) OTP {
+	if hasher == nil {
+		hasher = &Hasher{
+			HashName: "sha1",
+			Digest:   sha1.New,
+		}
+	}
+	return OTP{
+		secret: secret,
+		digits: digits,
+		hasher: hasher,
+	}
 }
