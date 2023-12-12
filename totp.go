8fd841cde86f591dea0c7b20ccbc6f5ad81872c7
pchchv <ipchchv@gmail.com>
2023-12-12 17:22:12 +0300

    Implement a timecode and At methods

--- totp.go
+++ totp.go
@@ -14,3 +14,12 @@ func NewTOTP(secret string, digits, interval int, hasher *Hasher) *TOTP {
 func NewDefaultTOTP(secret string) *TOTP {
 	return NewTOTP(secret, 6, 30, nil)
 }
+
+func (t *TOTP) timecode(timestamp int64) int64 {
+	return timestamp / int64(t.interval)
+}
+
+// Generate time OTP of given timestamp
+func (t *TOTP) At(timestamp int64) string {
+	return t.generateOTP(t.timecode(timestamp))
+}
