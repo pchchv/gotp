6e48a0384ef95bf00a217dc4e25203f6cbd386e2
pchchv <ipchchv@gmail.com>
2023-12-12 17:13:33 +0300

    Add an OTP type

--- otp.go
+++ otp.go
@@ -6,3 +6,9 @@ type Hasher struct {
 	HashName string
 	Digest   func() hash.Hash
 }
+
+type OTP struct {
+	secret string  // secret in base32 format
+	digits int     // number of integers in the OTP. Some apps expect this to be 6 digits, others support more.
+	hasher *Hasher // digest function to use in the HMAC (expected to be sha1)
+}
