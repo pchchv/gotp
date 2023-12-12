dfbc0d6684b717bd183ccbfe7310bce0b1debb35
pchchv <ipchchv@gmail.com>
2023-12-12 17:20:07 +0300

    Implement a NewTOTP and NewDefaultTOTP funcs

--- totp.go
+++ totp.go
@@ -5,3 +5,12 @@ type TOTP struct {
 	OTP
 	interval int
 }
+
+func NewTOTP(secret string, digits, interval int, hasher *Hasher) *TOTP {
+	otp := NewOTP(secret, digits, hasher)
+	return &TOTP{OTP: otp, interval: interval}
+}
+
+func NewDefaultTOTP(secret string) *TOTP {
+	return NewTOTP(secret, 6, 30, nil)
+}
