e3d9e15c421574fed41c35c71994321ee222e034
pchchv <ipchchv@gmail.com>
2023-12-12 17:32:28 +0300

    Implement a NewHOTP and NewDefaultHOTP funcs

--- hotp.go
+++ hotp.go
@@ -4,3 +4,13 @@ package otp
 type HOTP struct {
 	OTP
 }
+
+func NewHOTP(secret string, digits int, hasher *Hasher) *HOTP {
+	otp := NewOTP(secret, digits, hasher)
+	return &HOTP{OTP: otp}
+
+}
+
+func NewDefaultHOTP(secret string) *HOTP {
+	return NewHOTP(secret, 6, nil)
+}
