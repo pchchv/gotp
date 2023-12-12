bfc50aabb2f5ec683cf9dbba2cdd3ef25d5c463a
pchchv <ipchchv@gmail.com>
2023-12-12 17:23:09 +0300

    Implement a currentTimestamp function

--- utils.go
+++ utils.go
@@ -1,5 +1,7 @@
 package otp
 
+import "time"
+
 // integer to byte array
 func Itob(integer int64) []byte {
 	byteArr := make([]byte, 8)
@@ -8,4 +10,9 @@ func Itob(integer int64) []byte {
 		integer = integer >> 8
 	}
 	return byteArr
+}
+
+// get current timestamp
+func currentTimestamp() int64 {
+	return time.Now().Unix()
 }
