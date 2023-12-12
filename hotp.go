399167d097f237b7a84742b1484a24f5e4263baf
pchchv <ipchchv@gmail.com>
2023-12-12 17:33:35 +0300

    Implement an At and Verify methods

--- hotp.go
+++ hotp.go
@@ -14,3 +14,20 @@ func NewHOTP(secret string, digits int, hasher *Hasher) *HOTP {
 func NewDefaultHOTP(secret string) *HOTP {
 	return NewHOTP(secret, 6, nil)
 }
+
+// Generates the OTP for the given count.
+func (h *HOTP) At(count int) string {
+	return h.generateOTP(int64(count))
+}
+
+/*
+Verify OTP.
+
+params:
+
+	otp:   the OTP to check against
+	count: the OTP HMAC counter
+*/
+func (h *HOTP) Verify(otp string, count int) bool {
+	return otp == h.At(count)
+}
