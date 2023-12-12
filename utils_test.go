7700907ffb36a8f20203647cba831a6339a8ff29
pchchv <ipchchv@gmail.com>
2023-12-12 17:43:39 +0300

    Implement a test of an BuildUri function

--- utils_test.go
+++ utils_test.go
@@ -14,3 +14,34 @@ func TestITob(t *testing.T) {
 		t.Error("ITob error")
 	}
 }
+
+func TestBuildUri(t *testing.T) {
+	s := otp.BuildUri(
+		"totp",
+		"4S62BZNFXXSZLCRO",
+		"xlzd",
+		"SomeOrg",
+		"sha1",
+		0,
+		6,
+		0,
+	)
+	expected := "otpauth://totp/SomeOrg:xlzd?issuer=SomeOrg&secret=4S62BZNFXXSZLCRO"
+	if s != expected {
+		t.Errorf("BuildUri test failed.\n\texpected: %s,\n\tactual: %s", expected, s)
+	}
+
+	s2 := otp.BuildUri(
+		"hotp",
+		"XXSZLCRO4S62BZNF",
+		"mergenchik@gmail.com",
+		"github.com",
+		"sha1",
+		0,
+		6,
+		0)
+	expected2 := "otpauth://hotp/github.com:mergenchik@gmail.com?counter=0&issuer=github.com&secret=XXSZLCRO4S62BZNF"
+	if s2 != expected2 {
+		t.Errorf("BuildUri test failed.\n\texpected: %s,\n\tactual: %s", expected2, s2)
+	}
+}
