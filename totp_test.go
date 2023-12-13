e54f559c9d3e7b26f96c92a0165718c3bee993ed
pchchv <ipchchv@gmail.com>
2023-12-13 10:52:09 +0300

    Implement a test of a Verify method

--- totp_test.go
+++ totp_test.go
@@ -14,3 +14,9 @@ func TestTOTP_AtTime(t *testing.T) {
 		t.Error("TOTP at time generate otp error!")
 	}
 }
+
+func TestTOTP_Verify(t *testing.T) {
+	if !totp.Verify("179394", 1524485781) {
+		t.Error("verify faild")
+	}
+}
